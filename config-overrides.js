6cc3ec3f07595cfcc39584c65427b142323f7b96
a.chernets <alex.chernets@almexoft.com>
2020-03-22 22:21:13 +0200

    new config

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,16 @@
-const { override, addLessLoader, fixBabelImports } = require('customize-cra');
+const { override, addLessLoader, /*fixBabelImports*/ } = require('customize-cra');
 
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: true
-  }),
-  fixBabelImports('formik-antd', {
-    libraryName: 'formik-antd',
-    libraryDirectory: 'es',
-    style: 'css',
-  }),
+  // fixBabelImports('import', {
+  //   libraryName: 'antd',
+  //   libraryDirectory: 'es',
+  //   style: 'css'
+  // }),
+  // fixBabelImports('formik-antd', {
+  //   libraryName: 'formik-antd',
+  //   libraryDirectory: 'es',
+  //   style: 'css',
+  // }),
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: { '@font-family': 'Open Sans', '@border-radius-base': '4px' },
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import 'antd/dist/antd.less';
 import App from './pages/App';
 import ModalProvider from 'react-redux-modal-provider';
 import { Provider } from 'react-redux';
